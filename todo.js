cf72f3bff454ab8f7c3a13bf51a00cceb636ae2e
chihanglee <lujirk240@gmail.com>
2021-01-31 12:42:53 +0800

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,61 +2,73 @@ var fs = require('fs');
 const verb = process.argv[2]
 const content = process.argv[3]
 const content2 = process.argv[4]
+const dbPath = '/Users/mario/Desktop/xdml/node-demo-1/db'
 
+ensureDd()
+
+const n = content
+const list = fetch()
 switch (verb) {
     case 'add':
-        fs.stat('/Users/mario/Desktop/xdml/node-demo-1/db', function (err, stat) {
-            if (err == null) {
-                const fileContent = fs.readFileSync('/Users/mario/Desktop/xdml/node-demo-1/db').toString()
-                const list = JSON.parse(fileContent); // 反序列化
-
-                const task = content // 获取任务
-                list.push([task, false]) // 存入任务到 list
-                fs.writeFileSync('/Users/mario/Desktop/xdml/node-demo-1/db', JSON.stringify(list)) // 存到数据库
-                console.log(list) // 列出所有任务
-            } else if (err.code === 'ENOENT') {
-                fs.writeFileSync('/Users/mario/Desktop/xdml/node-demo-1/db', '')
-                const list = []
-
-                const tash = content // 获取任务
-                list.push([task, false]) // 存入任务到 list
-                fs.writeFileSync('/Users/mario/Desktop/xdml/node-demo-1/db', JSON.stringify(list)) // 存到数据库
-                console.log(list) // 列出所有任务
-            } else {
-                console.log('Some other error: ', err.code);
-            }
-        });
+        addTask(list, content)
         break;
     case 'list':
-        const fileContent = fs.readFileSync('/Users/mario/Desktop/xdml/node-demo-1/db').toString()
-        const list = JSON.parse(fileContent); // 反序列化
-        console.log(list)
         break;
     case 'delete':
-        const fileContent = fs.readFileSync('/Users/mario/Desktop/xdml/node-demo-1/db').toString()
-        const list = JSON.parse(fileContent); // 反序列化
-        const n = content // 从1开始
-        list.splice(n - 1, 1)
-        console.log(list)
-        fs.writeFileSync('/Users/mario/Desktop/xdml/node-demo-1/db', JSON.stringify(list)) // 存到数据库
+        removeTask(list, n)
         break;
     case 'done':
-        const fileContent = fs.readFileSync('/Users/mario/Desktop/xdml/node-demo-1/db').toString()
-        const list = JSON.parse(fileContent); // 反序列化
-        const n = content // 从1开始
-        list[n - 1][1] = true // ['接女神'，false]
-        console.log(list)
-        fs.writeFileSync('/Users/mario/Desktop/xdml/node-demo-1/db', JSON.stringify(list)) // 存到数据库
+        markTaskAsDone(list, n)
         break;
     case 'edit':
-        const fileContent = fs.readFileSync('/Users/mario/Desktop/xdml/node-demo-1/db').toString()
-        const list = JSON.parse(fileContent); // 反序列化
-        const n = content // 从1开始
-        list[n - 1][0] = content2
-        console.log(list)
-        fs.writeFileSync('/Users/mario/Desktop/xdml/node-demo-1/db', JSON.stringify(list)) // 存到数据库
+        editTask(list, n, content2)
         break;
     default:
         console.log('我不知道你想干啥')
         break;
+}
+
+display(list)
+if (verb !== 'list') {
+    save(list)
+}
+
+
+
+// 帮助函数
+function ensureDd() {
+    try {
+        fs.statSync(dbPath)
+    } catch (error) {
+        fs.writeFileSync(dbPath, '')
+    }
+}
+
+function save(list) {
+    fs.writeFileSync(dbPath, JSON.stringify(list)) // 存到数据库
+}
+function fetch() {
+    const fileContent = fs.readFileSync(dbPath).toString()
+    let list
+    try {
+        list = JSON.parse(fileContent) || [] // 兜底制
+    } catch (error) {
+        list = []
+    }
+    return list
+}
+function display(list) {
+    console.log(list) // 列出所有任务
+}
+function addTask(list, content) {
+    list.push([content, false])
+}
+function removeTask(list, n) {
+    list.splice(n - 1, 1)
+}
+function markTaskAsDone(list, n) {
+    list[n - 1][1] = true // ['接女神',false]
+}
+function editTask(list, n, newContent) {
+    list[n - 1][0] = newContent
 }
